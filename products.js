ee0da0fbe6df9b5f88fdd9bdfcb7366768dc750d
IG <iikim2511@gmail.com>
2021-12-08 15:31:04 -0500

    products.js

---
+++ products.js
@@ -0,0 +1,51 @@
+const products = [
+    {
+      id: 0,
+      name: "T-shirt 1",
+      price: 29.99,
+      instock: 9,
+      description: "inventory : 9",
+      imgSrc: "./img/Clothing-Fill-Color(1).png",
+    },
+    {
+      id: 1,
+      name: "T-shirt 2",
+      price: 24.99,
+      instock: 8,
+      description: "inventory : 8",
+      imgSrc: "./img/Clothing-Fill-Color(2).png",
+    },
+    {
+      id: 2,
+      name: "T-shirt 3",
+      price: 19.99,
+      instock: 10,
+      description: "inventory : 10",
+      imgSrc: "./img/Clothing-Fill-Color(3).png",
+    },
+    {
+      id: 3,
+      name: "T-shirt 4",
+      price: 25.99,
+      instock: 5,
+      description: "inventory : 5",
+      imgSrc: "./img/Clothing-Fill-Color(4).png",
+    },
+    {
+      id: 4,
+      name: "T-shirt 5",
+      price: 29.99,
+      instock: 4,
+      description: "inventory : 4",
+      imgSrc: "./img/Clothing-Fill-Color(5).png",
+    },
+    {
+      id: 5,
+      name: "T-shirt 6",
+      price: 39.99,
+      instock: 7,
+      description: "inventory : 7",
+      imgSrc: "./img/Clothing-Fill-Color(6).png",
+    },
+  ];
+  
